d99e3627accfbe96d5a36deb9727a6e7b1aff5f8
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-21 16:57:27 +0100

    es in Vue

--- js/script.js
+++ js/script.js
@@ -1,16 +1,59 @@
-const header = document.querySelector("h1");
-header.classList.add("text-center")
-header.innerHTML = "Lista di Mail";
+/*
+//INSERISCO NELL'ELEMENTO HEADER, L'ELEMENTO CREATO IN JS h1
+const header = document.querySelector("header");
+const h1 = document.createElement("h1");
+h1.classList.add("text-center")
+h1.innerHTML = "Lista di Mail";
+header.append(h1);
+
+//INSERISCO NELL'ELEMENTO BODY, L'ELEMENTO CREATO IN JS unOrderList
+const body = document.querySelector("body");
+const unOrderList = document.createElement("ul");
+body.append(unOrderList);
+
+//LITERAL array (lista di mail), VARIABILE result (in cui salviamo il risultato della chiamata axios)
 const mailList = [];
 let result;
 
-for (index = 0; index < 10; index++) {
+for (i = 0; i < 10; i++) {
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(function (response) {
         result = response.data.response;
         mailList.push(result);
-       
+
+        const listItem = document.createElement("li");
+        listItem.innerHTML = result;
+        unOrderList.append(listItem);
 
     });
 }
 
-console.log(mailList)
+console.log(mailList);
+*/
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            title: "Lista di Mail",
+            randomMail: [],
+        }
+    },
+
+    methods: {
+        getRandomMail: function() {
+            for (i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then( (response) => {
+                    console.log(response.data.response);
+                    
+                    this.randomMail.push(response.data.response);
+                })
+            }
+        }
+    },
+
+    created() {
+        this.getRandomMail();
+
+    }
+}).mount('#app')
